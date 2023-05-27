994ac7364b1bcdabdd427244481ce28ca7dc0e25
SuperAIdesu <tjx10011@gmail.com>
2023-05-27 19:39:01 -0400

    font

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -4,6 +4,15 @@ import theme from "./theme.js";
 export default defineUserConfig({
   base: "/",
 
+  head: [
+    ['link', { rel: 'stylesheet', href: '//unpkg.com/heti/umd/heti.min.css' }],
+    ['script', {}, 'var element = document.getElementsByClassName("theme-hope-content"); element[0].classList.add("heti");'],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap' }],
+    // ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap' }],
+    // ['script', {src: '//unpkg.com/heti/umd/heti-addon.min.js'}],
+    // ['script', {}, "const heti = new Heti('.heti'); heti.autoSpacing();"]
+  ],
+
   locales: {
     "/": {
       lang: "en-US",
