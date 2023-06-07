2a794354f3c147dc5ed56322414ef4f29eee3270
SuperAIdesu <tjx10011@gmail.com>
2023-06-06 21:23:24 -0400

    cf analytics

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,6 +8,7 @@ export default defineUserConfig({
     ['link', { rel: 'stylesheet', href: '//unpkg.com/heti/umd/heti.min.css' }],
     ['script', {}, 'var element = document.getElementsByClassName("theme-hope-content"); element[0].classList.add("heti");'],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Sans+TC&display=swap' }],
+    ['script', {src: 'https://static.cloudflareinsights.com/beacon.min.js', "data-cf-beacon": '{"token": "500ee885fc984594bd06da5556209b60"}'}]
     // ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif+TC&display=swap' }],
     // ['script', {src: '//unpkg.com/heti/umd/heti-addon.min.js'}],
     // ['script', {}, "const heti = new Heti('.heti'); heti.autoSpacing();"]
